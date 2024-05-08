885fea61dd404c755715d55bf8bee0d653911b09
yuri_floppočkin <shermanguyfrom50blesssings@gmail.com>
2024-05-08 13:40:26 +0300

    basics set up

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="site">
+        <header>
+            <div class="header_nav">
+                <div class="site_name">Имя Сайта</div>
+                <div><a href="#">Галерея</a></div>
+                <div><a href="#">Заказать услуги</a></div>
+            </div>
+        </header>
+        <div class="about_us">
+            <div id="B1E1"> <!-- BnEn - Блок n, Элемент n, так обозначаются элементы к которым обращается файл стиля, но которым разработик сайта посчитал не давать конкретного имени -->
+                <h2>Добро пожаловать на мой сайт-портфолио</h2><br>
+            </div>
+            <div id="B1E2">
+                <h2>Обо мне</h2><br><br>
+                <div class="B1E3">
+                    Приветствую вас в своем художественном архиве, где я представляю свою страсть и творческое путешествие.
+                    С ранних лет я ощущал непреодолимое влечение к искусству.
+                    Это увлечение побудило меня посвятить свою жизнь оттачиванию своего таланта, 
+                    получая классическое художественное образование и наставничество у выдающихся мастеров.
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
