e53567bac0ff8797fbd9ad2e5c045ba6e3090038
yuri_floppočkin <shermanguyfrom50blesssings@gmail.com>
2024-05-10 22:54:50 +0300

    uploading pics done

--- admin.php
+++ admin.php
@@ -1,5 +1,4 @@
 <?php
-require_once "vendor/connect.php";
 session_start();
 if(!isset($_SESSION["admin"])){
     header("Location:index.php");
@@ -14,6 +13,21 @@ if(!isset($_SESSION["admin"])){
     <title>Document</title>
 </head>
 <body>
-    ADMIN PANEL
+<div class="site">
+        <header><center><h1>Админская панель</h1></center><a href="vendor/logout.php">Выйти</a></header>
+        <div class="add_pic">
+            <h2>Загрузить изображение в галерею</h2><br>
+            <form action="vendor/pic_upload.php" enctype="multipart/form-data" method="post" class="add_pic_form">
+                <input type="file" name="image" multiple accept="image/*,image/png,image/jpeg"><br>
+                <input type="submit" value="Загрузить">
+            </form>
+            <?php
+                if(isset($_SESSION['upload_status'])){
+                    echo '<br><div class="msg">' . $_SESSION['upload_status'] .'</div>';
+                }
+                unset($_SESSION['upload_status']);
+            ?>
+        </div>
+    </div>
 </body>
 </html>
---
+++ vendor/pic_upload.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+require_once "connect.php";
+define('MB', 1048576);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if(empty($_FILES["image"]["name"])) { // Валидация в случае того, если пользователь забыл загрузить картинку и нажал на "загрузить"
+		$_SESSION['upload_status']='Выберите файл';
+        header('Location:../admin.php');
+        exit();
+    }
+
+    if($_FILES["image"]["size"] == 0) { // Валидация размера файла
+		$_SESSION['upload_status']='Файл слишком большой';
+        header('Location:../admin.php');
+        exit();
+    }
+
+    if($_FILES["image"]["size"] > 5*MB) { // Валидация размера файла
+		$_SESSION['upload_status']='Файл слишком большой';
+        header('Location:../admin.php');
+        exit();
+    }
+
+    $getFormat = explode('.', $_FILES["image"]["name"]); // Валидация формата файла
+	$format = strtolower(end($getFormat));
+	$types = array('jpg', 'png','jpeg');
+    
+	if(!in_array($format, $types)) {
+        $_SESSION['upload_status']='Недопустимый формат файла';
+        header('Location:../admin.php');
+        exit();
+    }
+
+    $path = "../vendor/uploads/";
+    $targetDir = "vendor/uploads/";
+    $image_path = $targetDir . basename($_FILES["image"]["name"]);
+
+	if (!@copy($_FILES['image']['tmp_name'], $path. $_FILES['image']['name'])) {
+        $_SESSION['upload_status']='Ошибка загрузки';
+        header('Location:../admin.php');
+        exit();
+    }
+	else {
+        $upload_date = date("M d Y", filemtime($_FILES['image']['tmp_name']));
+        $upload = mysqli_query($connect, "INSERT INTO `uploads` (`id`, `image_path`, `upload_date`) VALUES (NULL, '$image_path', '$upload_date')");
+        $_SESSION['upload_status']='Файл загружен';
+        header('Location:../admin.php');
+
+    }
+};
+
+?>
